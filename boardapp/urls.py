01d23276ce3e0535154bf3c9cdb8ce66456f9f57
hiroya <hiroya3508@gmail.com>
2023-11-23 03:51:31 +0900

    base.htmlとsignup画面の作成

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import signup_func
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('signup/', signup_func),
+
 ]
