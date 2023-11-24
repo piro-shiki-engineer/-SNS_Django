4f07c2f759ce8aca894930724f81cfabb43054f6
hiroya <hiroya3508@gmail.com>
2023-11-25 04:59:00 +0900

    ユーザー登録機能の追加

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 
 def signup_func(request):
-    return render(request, 'signup.html', {})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(username, '', password)
+    return render(request, 'signup.html', {'some': 100})
