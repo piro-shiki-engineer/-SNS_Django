63b741db5e6ee1f2237d3a7c1755b85f435e84bb
hiroya <hiroya3508@gmail.com>
2023-11-25 05:14:48 +0900

    ユーザーの重複防止の機能を追加

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-
+from django.db import IntegrityError
 
 def signup_func(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
-        user = User.objects.create_user(username, '', password)
+        try:
+            user = User.objects.create_user(username, '', password)
+            return render(request, 'signup.html', {'some': 100})
+
+        except IntegrityError:
+            return render(request, 'signup.html', {'error': 'このユーザーはすでに登録されています。'})
+
     return render(request, 'signup.html', {'some': 100})
